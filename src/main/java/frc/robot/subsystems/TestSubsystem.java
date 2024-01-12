8f6d837589ff95929a3c3b5be791da2e297dddb7
Elliot Scher <ecscher84@gmail.com>
2024-01-12 01:00:42 -0500

    changed to voltageout

--- src/main/java/frc/robot/subsystems/TestSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -1,18 +1,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
 public class TestSubsystem extends SubsystemBase {
-  private final TalonFX motor1 = new TalonFX(50);
+  private final TalonFX motor1 = new TalonFX(52);
   private final TalonFX motor2 = new TalonFX(51);
 
-  private final LoggedDashboardNumber motor1Speed = new LoggedDashboardNumber("Motor1 speed");
-  private final LoggedDashboardNumber motor2Speed = new LoggedDashboardNumber("Motor2 speed");
+  private final LoggedDashboardNumber motor1Volts = new LoggedDashboardNumber("Motor1 speed");
+  private final LoggedDashboardNumber motor2Volts = new LoggedDashboardNumber("Motor2 speed");
 
   public TestSubsystem() {
     var driveConfig = new TalonFXConfiguration();
@@ -22,10 +23,15 @@ public class TestSubsystem extends SubsystemBase {
     motor2.getConfigurator().apply(driveConfig);
   }
 
+  @Override
+  public void periodic() {
+    Logger.recordOutput("RPS", motor1.getVelocity().getValue());
+  }
+
   public Command runMotor1() {
     return startEnd(
         () -> {
-          motor1.setControl(new DutyCycleOut(motor1Speed.get()));
+          motor1.setControl(new VoltageOut(motor1Volts.get()));
         },
         () -> {
           motor1.stopMotor();
@@ -35,7 +41,7 @@ public class TestSubsystem extends SubsystemBase {
   public Command runMotor2() {
     return startEnd(
         () -> {
-          motor2.setControl(new DutyCycleOut(motor2Speed.get()));
+          motor2.setControl(new VoltageOut(motor2Volts.get()));
         },
         () -> {
           motor2.stopMotor();
@@ -45,8 +51,8 @@ public class TestSubsystem extends SubsystemBase {
   public Command runBoth() {
     return startEnd(
         () -> {
-          motor1.setControl(new DutyCycleOut(motor1Speed.get()));
-          motor2.setControl(new DutyCycleOut(motor2Speed.get()));
+          motor1.setControl(new VoltageOut(motor1Volts.get()));
+          motor2.setControl(new VoltageOut(motor2Volts.get()));
         },
         () -> {
           motor1.stopMotor();
