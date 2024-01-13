26ec1679766a31b2862861112c26d86fbc60a084
Elliot Scher <ecscher84@gmail.com>
2024-01-12 23:18:41 -0500

    changed to velocity control with voltage compensation

--- src/main/java/frc/robot/subsystems/TestSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -1,7 +1,8 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,26 +13,43 @@ public class TestSubsystem extends SubsystemBase {
   private final TalonFX motor1 = new TalonFX(52);
   private final TalonFX motor2 = new TalonFX(51);
 
-  private final LoggedDashboardNumber motor1Volts = new LoggedDashboardNumber("Motor1 speed");
-  private final LoggedDashboardNumber motor2Volts = new LoggedDashboardNumber("Motor2 speed");
+  private final LoggedDashboardNumber speed = new LoggedDashboardNumber("speed");
+  private final LoggedDashboardNumber p = new LoggedDashboardNumber("p", 0.1);
+  private final LoggedDashboardNumber i = new LoggedDashboardNumber("i", 0);
+  private final LoggedDashboardNumber d = new LoggedDashboardNumber("d", 0.001);
+  private final LoggedDashboardNumber v = new LoggedDashboardNumber("v", 0.115);
+
+  private final VelocityVoltage velocity = new VelocityVoltage(0);
+
+  private final Slot0Configs config = new Slot0Configs();
 
   public TestSubsystem() {
     var driveConfig = new TalonFXConfiguration();
     driveConfig.CurrentLimits.StatorCurrentLimit = 40.0;
     driveConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+
     motor1.getConfigurator().apply(driveConfig);
     motor2.getConfigurator().apply(driveConfig);
   }
 
   @Override
   public void periodic() {
-    Logger.recordOutput("RPS", motor1.getVelocity().getValue());
+    Logger.recordOutput("motor 1", motor1.getVelocity().getValueAsDouble());
+    Logger.recordOutput("motor 2", motor2.getVelocity().getValueAsDouble());
+
+    config.kV = v.get();
+    config.kP = p.get();
+    config.kI = i.get();
+    config.kD = d.get();
+
+    motor1.getConfigurator().apply(config);
+    motor2.getConfigurator().apply(config);
   }
 
   public Command runMotor1() {
     return startEnd(
         () -> {
-          motor1.setControl(new VoltageOut(motor1Volts.get()));
+          motor1.setControl(velocity.withVelocity(speed.get()).withSlot(0));
         },
         () -> {
           motor1.stopMotor();
@@ -41,7 +59,7 @@ public class TestSubsystem extends SubsystemBase {
   public Command runMotor2() {
     return startEnd(
         () -> {
-          motor2.setControl(new VoltageOut(motor2Volts.get()));
+          motor2.setControl(velocity.withVelocity(speed.get()).withSlot(0));
         },
         () -> {
           motor2.stopMotor();
@@ -51,8 +69,8 @@ public class TestSubsystem extends SubsystemBase {
   public Command runBoth() {
     return startEnd(
         () -> {
-          motor1.setControl(new VoltageOut(motor1Volts.get()));
-          motor2.setControl(new VoltageOut(motor2Volts.get()));
+          motor1.setControl(velocity.withVelocity(speed.get()).withSlot(0));
+          motor2.setControl(velocity.withVelocity(speed.get()).withSlot(0));
         },
         () -> {
           motor1.stopMotor();
