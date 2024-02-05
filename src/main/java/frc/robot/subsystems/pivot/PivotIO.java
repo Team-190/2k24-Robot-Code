0b3539135612e7206a8e39cee6e0dd336436bbf5
Elliot Scher <ecscher84@gmail.com>
2024-02-04 19:39:06 -0500

    PivotIO Rotation2D initialize

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -6,9 +6,9 @@ import org.littletonrobotics.junction.AutoLog;
 public interface PivotIO {
   @AutoLog
   public static class PivotIOInputs {
-    public Rotation2d position;
-    public double velocityRadPerSec;
-    public double appliedVolts;
+    public Rotation2d position = new Rotation2d();
+    public double velocityRadPerSec = 0.0;
+    public double appliedVolts = 0.0;
     public double[] currentAmps = new double[] {};
     public double[] tempCelcius = new double[] {};
   }
