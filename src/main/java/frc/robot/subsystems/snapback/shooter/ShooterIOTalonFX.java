7d100f57c0048c74470da97cc4b7f8edc51a6ebe
anshu <aradig26@students.shrewsbury.k12.ma.us>
2024-11-04 18:20:18 -0500

    update shooterIO to 2025

--- src/main/java/frc/robot/subsystems/snapback/shooter/ShooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/snapback/shooter/ShooterIOTalonFX.java
@@ -5,33 +5,39 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.util.Alert;
 import frc.robot.util.Alert.AlertType;
 
 public class ShooterIOTalonFX implements ShooterIO {
   private final TalonFX leftFlywheel;
-  public StatusSignal<Double> leftPosition;
-  public StatusSignal<Double> leftVelocityRadiansPerSecond;
-  public StatusSignal<Double> leftAppliedVolts;
-  public StatusSignal<Double> leftCurrentAmps;
-  public StatusSignal<Double> leftTemperatureCelsius;
+  public StatusSignal<Angle> leftPosition;
+  public StatusSignal<AngularVelocity> leftVelocityRadiansPerSecond;
+  public StatusSignal<Voltage> leftAppliedVolts;
+  public StatusSignal<Current> leftCurrentAmps;
+  public StatusSignal<Temperature> leftTemperatureCelsius;
 
   private final TalonFX rightFlywheel;
-  public StatusSignal<Double> rightPosition;
-  public StatusSignal<Double> rightVelocityRadiansPerSecond;
-  public StatusSignal<Double> rightAppliedVolts;
-  public StatusSignal<Double> rightCurrentAmps;
-  public StatusSignal<Double> rightTemperatureCelsius;
+  public StatusSignal<Angle> rightPosition;
+  public StatusSignal<AngularVelocity> rightVelocityRadiansPerSecond;
+  public StatusSignal<Voltage> rightAppliedVolts;
+  public StatusSignal<Current> rightCurrentAmps;
+  public StatusSignal<Temperature> rightTemperatureCelsius;
 
   private final TalonFX accelerator;
-  public StatusSignal<Double> acceleratorPosition;
-  public StatusSignal<Double> acceleratorVelocityRadiansPerSecond;
-  public StatusSignal<Double> acceleratorAppliedVolts;
-  public StatusSignal<Double> acceleratorCurrentAmps;
-  public StatusSignal<Double> acceleratorTemperatureCelsius;
+  public StatusSignal<Angle> acceleratorPosition;
+  public StatusSignal<AngularVelocity> acceleratorVelocityRadiansPerSecond;
+  public StatusSignal<Voltage> acceleratorAppliedVolts;
+  public StatusSignal<Current> acceleratorCurrentAmps;
+  public StatusSignal<Temperature> acceleratorTemperatureCelsius;
 
   private final Alert leftDisconnectedAlert =
       new Alert("Shooter left Talon is disconnected, check CAN bus.", AlertType.ERROR);
@@ -47,18 +53,18 @@ public class ShooterIOTalonFX implements ShooterIO {
     rightFlywheel = new TalonFX(ShooterConstants.RIGHT_FLYWHEEL_MOTOR_CAN_ID);
     accelerator = new TalonFX(ShooterConstants.ACCELERATOR_MOTOR_CAN_ID);
 
-    TalonFXConfiguration config = new TalonFXConfiguration();
-    config.CurrentLimits.SupplyCurrentLimit = 60.0;
-    config.CurrentLimits.SupplyCurrentLimitEnable = true;
-    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    TalonFXConfiguration rightFlywheelConfig = new TalonFXConfiguration();
+    rightFlywheelConfig.CurrentLimits.SupplyCurrentLimit = 60.0;
+    rightFlywheelConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    rightFlywheelConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    rightFlywheelConfig.Feedback.SensorToMechanismRatio = ShooterConstants.FLYWHEEL_GEAR_RATIO;
+    rightFlywheel.getConfigurator().apply(rightFlywheelConfig);
 
-    TalonFXConfiguration flywheelConfig = config;
-    flywheelConfig.Feedback.SensorToMechanismRatio = ShooterConstants.FLYWHEEL_GEAR_RATIO;
-    leftFlywheel.getConfigurator().apply(flywheelConfig);
-    leftFlywheel.setInverted(true);
-    rightFlywheel.getConfigurator().apply(flywheelConfig);
+    TalonFXConfiguration leftFlywheelConfig = rightFlywheelConfig;
+    leftFlywheelConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    leftFlywheel.getConfigurator().apply(leftFlywheelConfig);
 
-    TalonFXConfiguration acceleratorConfig = config;
+    TalonFXConfiguration acceleratorConfig = rightFlywheelConfig;
     acceleratorConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
     acceleratorConfig.Feedback.SensorToMechanismRatio = ShooterConstants.ACCELERATOR_GEAR_RATIO;
     accelerator.getConfigurator().apply(acceleratorConfig);
