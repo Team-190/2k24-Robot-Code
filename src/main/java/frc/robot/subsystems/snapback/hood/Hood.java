5c5bb5b67c6e3f5ec96e80cac8674fe39a58f77b
Sriaditya Vaddadi <vaddadi.sriaditya@gmail.com>
2024-11-11 18:30:59 -0500

    Provisional Work on the Hood Subsystem for Snapback
    Co-authored-by: Aradig26 <Aradig26@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/snapback/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/snapback/hood/Hood.java
@@ -1,3 +1,14 @@
 package frc.robot.subsystems.snapback.hood;
 
-public class Hood {}
+import frc.robot.util.LoggedTunableNumber;
+
+public class Hood extends SubsystemBase {
+
+    private final HoodIO io;
+    private final HoodIOInputsAutoLogged inputs  = new HoodIOInputsAutoLogged();
+
+    public Hood(HoodIO io) {
+        this.io = io;
+    }
+
+}
--- src/main/java/frc/robot/subsystems/snapback/hood/HoodIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/snapback/hood/HoodIOTalonFX.java
@@ -1,3 +1,60 @@
 package frc.robot.subsystems.snapback.hood;
 
-public class HoodIOTalonFX {}
+public class HoodIOTalonFX implements HoodIO {
+    private final TalonFX hoodMotor;
+    public StatusSignal<Angle> positionRadians;
+    public StatusSignal<Velocity> velocityRadiansPerSecond;
+    public StatusSignal<Current> currentAmps;
+    public StatusSignal<Temprature> tempratureCelsius;
+
+    private final Alert disconnectedAlert = new Alert("Hood Talon is disconnected, check CAN bus.", AlertType.ERROR);
+
+    private VoltageOut voltageControl;
+    private MotionMagicVoltage positionControl = new MotionMagicVoltage(0);
+
+    public HoodIOTalonFX() {
+        hoodMotor = new TalonFX(HoodConstants.HOOD_MOTOR_CAN_ID);
+
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.CurrentLimits.SupplyCurrentLimit = 60.0;
+        config.CurrentLimits.SupplyCurrentLimitEnable = true;
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        config.Feedback.SensorToMechanismRatio = HoodConstants.INTAKE_GEAR_REDUCTION;
+        config.Slot0.kP = HoodConstants.GAINS.kp();
+        config.Slot0.kI = HoodConstants.GAINS.ki();
+        config.Slot0.kD = HoodConstants.GAINS.kd();
+        config.Slot0.kS = HoodConstants.GAINS.ks();
+        config.Slot0.kV = HoodConstants.GAINS.kv();
+        config.Slot0.kA = HoodConstants.GAINS.ka();
+        hoodMotor.getConfigurator.apply(config);
+
+        positionRadians = hoodMotor.getPosition();
+        velocityRadiansPerSecond = hoodMotor.getVelocity();
+        currentAmps = hoodMotor.getCurrent();
+        tempratureCelsius = hoodMotor.getTemprature();
+
+        BaseStatusSignal.setUpdateFrequencyAll(50, positionRadians, velocityRadiansPerSecond, currentAmps,
+                tempratureCelsius);
+        hoodMotor.optimizeBusUtilization();
+    }
+
+    public void updateInputs(HoodIOInputs inputs) {
+        boolean isConnected = hoodMotor.refreshAll(
+                positionRadians, velocityRadiansPerSecond, currentAmps, tempratureCelsius).isOK();
+        disconnectedAlert.set(!isConnected);
+
+        inputs.position = positionRadians.getValueAsDouble();
+        inputs.velocity = velocityRadiansPerSecond.getValueAsDouble();
+        inputs.current = currentAmps.getValueAsDouble();
+        inputs.temprature = tempratureCelsius.getValueAsDouble();
+    }
+
+    public void setVoltage(double voltage) {
+        hoodMotor.setControl(voltageControl.withOutput(volts).withEnableFOC(true));
+    }
+
+    public void setPosition(double position) { // position in rotations!!!
+        hoodMotor.setControl(positionControl.withPosition(position).withEnableFOC(true));
+    }
+
+}
