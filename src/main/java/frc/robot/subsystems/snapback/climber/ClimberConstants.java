994d621b25b98500d5c41ed2af2500d6a8cc1eff
anshu <aradig26@students.shrewsbury.k12.ma.us>
2024-11-18 19:32:35 -0500

    add some constants in ClimberConstants

--- src/main/java/frc/robot/subsystems/snapback/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/snapback/climber/ClimberConstants.java
@@ -1,3 +1,38 @@
 package frc.robot.subsystems.snapback.climber;
 
-public class ClimberConstants {}
+import frc.robot.constants.Constants;
+
+public class ClimberConstants {
+    public static final int LEFT_CLIMBER_CAN_ID;
+    public static final int RIGHT_CLIMBER_CAN_ID;
+
+    public static final int LEFT_CLIMBER_SOLENOID_FORWARD;
+    public static final int LEFT_CLIMBER_SOLENOID_REVERSE;
+    public static final int RIGHT_CLIMBER_SOLENOID_FORWARD;
+    public static final int RIGHT_CLIMBER_SOLENOID_REVERSE;
+
+    static {
+        switch (Constants.ROBOT) {
+            case SNAPBACK:
+                LEFT_CLIMBER_CAN_ID = 0;
+                RIGHT_CLIMBER_CAN_ID = 1;
+                
+                LEFT_CLIMBER_SOLENOID_FORWARD = 0;
+                LEFT_CLIMBER_SOLENOID_REVERSE = 1;
+                RIGHT_CLIMBER_SOLENOID_FORWARD = 2;
+                RIGHT_CLIMBER_SOLENOID_REVERSE = 3;
+                break;
+        
+            default:
+                LEFT_CLIMBER_CAN_ID = 0;
+                RIGHT_CLIMBER_CAN_ID = 1;
+
+                LEFT_CLIMBER_SOLENOID_FORWARD = 0;
+                LEFT_CLIMBER_SOLENOID_REVERSE = 1;
+                RIGHT_CLIMBER_SOLENOID_FORWARD = 2;
+                RIGHT_CLIMBER_SOLENOID_REVERSE = 3;
+                break;
+        }
+    }
+
+}
